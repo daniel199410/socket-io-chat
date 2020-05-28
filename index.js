659b0f51983e7142c3ebbbd6152aff4eefee5bc8
Daniel Cataño <dcatano@cidenet.com.co>
2020-05-28 16:39:06 -0500

    Implementación del chat

--- index.js
+++ index.js
@@ -9,5 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const server = app.listen(app.get('port'), () => console.log('Server on port', app.get('port')));
 
 const io = SocketIO(server);
-
-io.on('connection', socket => console.log('New connection', socket.id));
+io.on('connection', socket => {
+    socket.on('chat.message', data => io.sockets.emit('chat.message', data));
+    socket.on('chat.typing', data => socket.broadcast.emit('chat.typing', data));
+});
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,19 @@
 const socket = io();
+const message = document.getElementById('message');
+const username = document.getElementById('username');
+const sendButton = document.getElementById('send');
+const output = document.getElementById('output');
+const actions = document.getElementById('actions');
+
+sendButton.addEventListener('click', () => socket.emit('chat.message', {
+    message: message.value,
+    username: username.value
+}));
+
+message.addEventListener('keypress', () => socket.emit('chat.typing', username.value));
+
+socket.on('chat.message', data => {
+    actions.innerHTML = '';
+    output.innerHTML += `<p><strong>${data.username}:</strong> ${data.message}</p>`
+});
+socket.on('chat.typing', data => actions.innerHTML = `<p><em></em>${data}</em> is typing a message</p>`)
