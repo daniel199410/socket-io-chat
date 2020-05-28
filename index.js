a8dc0753537580120a085451de6c1935247c8ed2
Daniel Cataño <dcatano@cidenet.com.co>
2020-05-28 15:36:54 -0500

    Boilerplate

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hola');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), () => console.log('Server on port', app.get('port')));
